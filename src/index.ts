a2aea457aecbb57ce37d195ab158abda0789bb5f
Syed Ahkam <smahkam57@gmail.com>
2023-03-14 02:28:36 +0530

    Export destroyKey as public API

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./checkAccess.js";
 export * from "./constants.js";
+export * from "./destroyKey.js";
 export * from "./generateKey.js";
 export * from "./sellKey.js";
 export * from "./types/idl.js";
